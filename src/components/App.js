eaa6f88da482df837ae68f88642fa4f1a0811c9f
SACHIN KUMAR <sachinlaish@gmail.com>
2020-12-02 12:27:38 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,37 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+
+    constructor(props){
+
+        super(props);
+
+        this.state = {
+            time: new Date()
+        }
+
+    }
+
+    update(){
+        this.setState({time: new Date()})
+    }
+
+    componentDidMount(){
+        setInterval(() => this.update(),1000);
+    }
+
     render() {
 
         return(
-            <>
+            <div className="Clock">
+
+                <h3 id="time">
+
+                    {this.state.time.toLocaleTimeString()}
+
+                </h3>
                
-            </>
+            </div>
         )
     }
 }
